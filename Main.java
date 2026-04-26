6f53e4565e5c8ebf7f369c8f0ee8efbbd10f33c3
SweetNoemi <ncernae@email.com>
2026-04-26 18:10:41 -0500

    Version 4: soporte para coeficientes decimales usando double

--- Main.java
+++ Main.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a, b, c;
+
+        double a, b, c;
         double discriminante, x1, x2;
 
         System.out.print("Ingrese a: ");
-        a = sc.nextInt();
+        a = sc.nextDouble();
 
         System.out.print("Ingrese b: ");
-        b = sc.nextInt();
+        b = sc.nextDouble();
 
         System.out.print("Ingrese c: ");
-        c = sc.nextInt();
+        c = sc.nextDouble();
 
         if (a == 0) {
             System.out.println("No es una ecuacion de segundo grado porque a no puede ser 0.");
@@ -22,19 +24,21 @@ public class Main {
         discriminante = (b * b) - (4 * a * c);
 
         if (discriminante > 0) {
-        x1 = (-b + (int)Math.sqrt((b*b)-(4*a*c))) / (2*a);
-        x2 = (-b - (int)Math.sqrt((b*b)-(4*a*c))) / (2*a);
+            x1 = (-b + Math.sqrt(discriminante)) / (2.0 * a);
+            x2 = (-b - Math.sqrt(discriminante)) / (2.0 * a);
 
-        System.out.println("Dos raices reales diferentes:");
-        System.out.println("Raiz 1: " + x1);
-        System.out.println("Raiz 2: " + x2);
+            System.out.println("Dos raices reales diferentes:");
+            System.out.println("Raiz 1: " + x1);
+            System.out.println("Raiz 2: " + x2);
+
         } else if (discriminante == 0) {
             x1 = (-b) / (2.0 * a);
 
             System.out.println("Una raiz real doble:");
             System.out.println("Raiz: " + x1);
-        } else{
+
+        } else {
             System.out.println("No existen raices reales.");
         }
-}
+    }
 }
